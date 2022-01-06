90ef426610b479015cfbe84c121ba7d57e253832
liuyunli <charis.wang@tron.network>
2022-01-06 14:18:23 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 
 // import TronWeb from './tronweb/src/index.js'
-const TronWeb = require('tronweb')
-// const TronWeb = require('./tronweb/dist/TronWeb.node');
+// const TronWeb = require('tronweb')
+const TronWeb = require('./tronweb/dist/TronWeb.node');
 const abiTest = require('./test/mainChain/abi.test');
 const base58Test = require('./test/mainChain/base58.test');
 const bytesTest = require('./test/mainChain/bytes.test');
@@ -28,6 +28,8 @@ const trc10Test = require('./test/sideChain/trc10.test.js');
 const trc20Test = require('./test/sideChain/trc20.test.js');
 const trc721Test = require('./test/sideChain/trc721.test.js');
 const trxTestSide = require('./test/sideChain/trx.test.js');
+const isConstantCall = require('./test/mainChain/isConstantCall.test.js');
+const contractTypes = require('./test/mainChain/contractTypes.test.js');
 const tronWebBuilder = require('./test/util/tronWebBuilder');
 
 
@@ -43,6 +45,8 @@ class App extends React.Component {
      */
     await multiSignTestMain.multiSignTestAll();
     // await multiSignTestSide.multiSignTestAll();
+    // await isConstantCall.isConstantCallTestAll();
+    // await contractTypes.contractTypesTestAll();
     // await trxTestSide.trxTestAll();
     // await trc721Test.trc721TestAll();
     // await trc20Test.trc20TestAll();
@@ -53,22 +57,23 @@ class App extends React.Component {
     // await bytesTest.bytesTestAll();
     // await codeTest.codeTestAll();
     // await methodTest.methodTestAll();
-    await pluginTest.pluginTestAll();
+    // await pluginTest.pluginTestAll();
     // await providersTest.providersTestAll();
    // await transactionBuilderTest.transactionBuilderTestAll();
    //  await trxTestMain.trxTestAll();
    //  await fallbackTest.fallbackTestAll();
 
+
     /** need nile environment
      *  Use react default domain name in the browser
      */
     // await eventTest.eventTestAll();
-    await indexTest.indexTestAll();
+    // await indexTest.indexTestAll();
 
     /** need fullHost: TEST_TRON_GRID_API environment
      *  Use http://trongrid.myservice.com domain name in the browser
      */
-    await trongridTest.trongridTestAll();
+    // await trongridTest.trongridTestAll();    运行时会有很多url返回401用例
     console.log("success all")
   }
 
